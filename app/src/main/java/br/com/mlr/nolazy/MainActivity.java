81c2067dcfa067ce8faa88e311f5d7b84b522956
RodrigoDiasSoares <64237149+RodrigoDiasSoares@users.noreply.github.com>
2020-11-26 22:01:51 -0300

    Começando o RecyclerView

--- app/src/main/java/br/com/mlr/nolazy/MainActivity.java
+++ app/src/main/java/br/com/mlr/nolazy/MainActivity.java
@@ -1,14 +1,24 @@
 package br.com.mlr.nolazy;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView recyclerHorarios;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerHorarios = findViewById(R.id.recyclerHorarios);
+
+        //adapter
+
+        //set LayoutManager do recyclerView
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getApplicationContext());
+        recyclerHorarios.setLayoutManager(layoutManager);
+        //recyclerHorarios.setAdapter();
     }
 }
